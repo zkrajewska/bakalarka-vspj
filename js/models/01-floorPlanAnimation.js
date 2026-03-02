66fa95125d08a989793b70cc11dee3c6f1e2a97f
zkrajewska <nap29bal@gmail.com>
2026-03-02 20:10:38 +0100

    feat: animate 1 and 4 models

--- js/models/01-floorPlanAnimation.js
+++ js/models/01-floorPlanAnimation.js
@@ -1,4 +1,40 @@
 export function initFloorPlan() {
+    const tl = gsap.timeline();
 
+    const GrainForCleaner = document.querySelectorAll(".grain-for-cleaner");
 
+    gsap.set(GrainForCleaner, { strokeDasharray: "12.112, 9.69" });
+
+    gsap.to(GrainForCleaner  , {
+        strokeDashoffset: -21.802,
+        duration: 1,
+        ease: "none", 
+        repeat: -1
+    });
+
+    const piles = document.querySelectorAll("#grain-mounds-container > g");
+
+    tl.fromTo(piles,
+        {scaleX: 0, scaleY: 0, transformOrigin: "50% 100%" },
+        {scaleX: 1, scaleY: 1, duration: 10, stagger: 5, ease: "steps.out", repeat: -1 }, "<"
+    );
+
+    const wheel = document.querySelectorAll(".wheel-part");
+
+    gsap.to(wheel,
+        {rotation: 360, ease: "none",duration: 10, repeat: -1, transformOrigin: "center"}
+    );
+
+    gsap.to(flames, 
+        {scaleY: 1.1, scaleX: 0.8, transformOrigin: "50% 100%", duration: 0.4, repeat: -1, yoyo: "true", ease: "rough"}
+    );
+
+    const elevator = document.querySelectorAll("#freight-elevator > g");
+
+    const liftTl = gsap.timeline({ repeat: -1, yoyo: true });
+
+    liftTl.to("#freight-elevator",
+        { y: "100%", duration: 8, ease: "none"}
+    );
+    liftTl.to("#freight-elevator", { duration: 5, autoAlpha: 0, delay: 2}, "<" );
 }
--- js/models/04-maltingFloor.js
+++ js/models/04-maltingFloor.js
@@ -1,4 +1,78 @@
 export function initMaltingFloor() {
 
+        const tl = gsap.timeline();
 
+    const GrainForCleaner = document.querySelectorAll(".grain-for-cleaner1");
+
+    gsap.set(GrainForCleaner, { strokeDasharray: "12.112, 9.69" });
+
+    gsap.to(GrainForCleaner  , {
+        strokeDashoffset: -21.802,
+        duration: 1,
+        ease: "none", 
+        repeat: -1
+    });
+
+    
+
+    const wheel = document.querySelectorAll(".wheel-part");
+
+    gsap.to(wheel,
+        {rotation: 360, ease: "none",duration: 10, repeat: -1, transformOrigin: "center"}
+    );
+
+    gsap.to(flames, 
+        {scaleY: 1.1, scaleX: 0.8, transformOrigin: "50% 100%", duration: 0.4, repeat: -1, yoyo: "true", ease: "rough"}
+    );
+
+    const cartPile = [ "#cart-pile1", "#cart-pile2" ];
+    const elevator = document.querySelectorAll("#freight-elevator > g");
+    const cart = document.querySelectorAll("#transport-cart");
+    const elevatorCargo = [ "#pile-left", "#pile-right" ]; 
+    const piles = document.querySelectorAll("#grain-mounds-container > g");
+
+    const liftTl = gsap.timeline({ repeat: -1});
+
+    
+
+    liftTl.fromTo(piles,
+        {scaleX: 0, scaleY: 0, transformOrigin: "50% 100%" },
+        {scaleX: 1, scaleY: 1, duration: 8, stagger: 2, ease: "steps.out" }
+    );
+
+    liftTl.fromTo(cartPile,
+        {scaleX: 0, scaleY: 0, transformOrigin: "50% 100%" },
+        {scaleX: 1, scaleY: 1, duration: 2, stagger: 0.5, ease: "none" }
+    );
+
+    liftTl.to(cart,
+        { x: +550, duration: 5 }
+    );
+
+    liftTl.to(cartPile,
+        { scale: 0, duration: 2, ease: "none", transformOrigin: "50% 100%" }
+    );
+
+    liftTl.fromTo(elevatorCargo,
+        {scaleX: 0, scaleY: 0, transformOrigin: "50% 100%" },
+        {scaleX: 1, scaleY: 1, duration: 2, stagger: 0.5, ease: "none" }
+    );
+
+    liftTl.to("#freight-elevator",
+        { y: "100%", duration: 8, ease: "none"}
+    );
+    liftTl.to("#freight-elevator", { duration: 5, autoAlpha: 0, yoyo: true}, "<" );
+
+
+    liftTl.set(cart,
+        {  scaleX: -1, duration: 2, stagger: 1, transformOrigin: "50% 50%"}
+    );
+
+    liftTl.to(cart,
+        { x: 0, duration: 5 }
+    );
+
+    liftTl.set(cart,
+        {  scaleX: -1, duration: 2 }
+    );
 }
